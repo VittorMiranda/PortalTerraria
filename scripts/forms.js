1c0e24581ea92aa181821082fcda61469f37284b
VITOR HUGO MIRANDA <vitor.hmiranda@senacsp.edu.br>
2025-06-10 17:12:57 -0300

    Adicionado card para pré visualização do formulario e imagens iniciais para aba de flora, organização de imagens de fauna em pasta separada.

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,24 +1,42 @@
+let arquivoSelecionado = null;
 
+// Captura o arquivo selecionado e armazena em uma variável
 const inputFile = document.getElementById('upload');
-
 inputFile.addEventListener('change', event => {
-  const file = event.target.files[0];
-  if (file) {
-    const url = URL.createObjectURL(file);
-    document.getElementById("temp").innerHTML = `<img src="${url}" alt="Pré-visualização" width="200">`;
-  }
+  arquivoSelecionado = event.target.files[0];
 });
 
-function dados(){
-    let escolha
-    let nome = document.getElementById("nome");
-    let dataAtualizada = document.getElementById("data-atualizacao");
-    let tipos = document.getElementsByName("escolha");
-    Array.from(tipos).forEach(tipo => {
+function dados() {
+  // Coleta os valores do formulário
+  let nome = document.getElementById("nome").value;
+  let dataAtualizada = document.getElementById("data-atualizacao").value;
+  let informacao = document.getElementById("informacoes").value;
+  let escolha;
+  let tipos = document.getElementsByName("escolha");
+  Array.from(tipos).forEach(tipo => {
     if (tipo.checked) {
-        escolha = tipo.value;
+      escolha = tipo.value;
     }
-    });
-    let informacao = document.getElementById("informacoes");
-    
+  });
+
+  // Exibe a imagem se um arquivo tiver sido selecionado
+  if (arquivoSelecionado) {
+    const url = URL.createObjectURL(arquivoSelecionado);
+    document.getElementById("temp").innerHTML = `<div class="card">
+                    <h3 class="titulo-card">${nome}</h3>
+                    <img src="${url}" alt="" class="imagem-card" width="20px">
+                    <p class="conteudo-card">
+                        ${informacao}
+                    </p>
+                </div>`;
+  } else {
+    document.getElementById("temp").innerHTML = `<div class="card">
+                    <h3 class="titulo-card">${nome}</h3>
+                    <img src="" alt="" class="imagem-card" width="20px">
+                    <p class="conteudo-card">
+                        ${informacao}
+                    </p>
+                </div>`;
+  }
+
 }
